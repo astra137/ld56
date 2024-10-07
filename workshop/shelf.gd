1631841541fe5b5b48a76e393c71df5cd08fc2d4
vandomej <jacob.vandome15@gmail.com>
2024-10-07 13:38:38 -0700

    Adding labels to elements

--- workshop/shelf.gd
+++ workshop/shelf.gd
@@ -3,6 +3,8 @@ extends StaticBody2D
 const BOTTLE = preload('res://workshop/bottle.tscn')
 const ROW1 = -132
 const ROW2 = -44
+const ROW3 = 44
+const ROW4 = 132
 
 
 var bottles: Array[RigidBody2D] = []
@@ -18,13 +20,21 @@ func idx_to_pos(idx: int) -> Vector2:
 		5: return Vector2(-44, -44)
 		6: return Vector2(44, -44)
 		7: return Vector2(132, -44)
+		8: return Vector2(-132, 44)
+		9: return Vector2(-44, 44)
+		10: return Vector2(44, 44)
+		11: return Vector2(132, 44)
+		12: return Vector2(-132, 132)
+		13: return Vector2(-44, 132)
+		14: return Vector2(44, 132)
+		15: return Vector2(132, 132)
 		_: return Vector2.ZERO
 
 
 func spawn_bottle(idx: int) -> RigidBody2D:
 	var bottle: RigidBody2D = BOTTLE.instantiate()
 	bottle.tree_exiting.connect(handle_bottle_gone.bind(idx))
-	bottle.type = (idx % 4) as Furble.CreatureTypes
+	bottle.type = floor(idx / 4) as Furble.CreatureTypes
 	bottle.position = idx_to_pos(idx)
 	%Bottles.add_child.call_deferred(bottle)
 	bottles[idx] = bottle
@@ -38,5 +48,5 @@ func handle_bottle_gone(idx: int) -> void:
 
 
 func _ready() -> void:
-	bottles.resize(8)
-	for idx in 8: spawn_bottle(idx)
+	bottles.resize(16)
+	for idx in 16: spawn_bottle(idx)
